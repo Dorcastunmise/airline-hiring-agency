d97ccc25fe1c0279ee6b155bf93d3b039cf55113
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-06-20 19:04:48 +0100

    adding a new apge of html contents

--- hostess.js
+++ hostess.js
@@ -1,5 +1,5 @@
-var canvas = document.getElementById("circlecanvas");
+var canvas = document.getElementById("circleCanvas");
 var context = canvas.getContext("2d");
 context.arc(50, 50, 50, 0, Math.PI * 2, false);
 context.fillStyle = "red";
-context.fill()
+context.fill();
