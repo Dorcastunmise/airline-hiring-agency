37cf267ed50d9693283c6f45fc17c15e52346afb
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-06-24 19:32:15 +0100

    replacing home page with another

--- hostess.js
+++ hostess.js
@@ -1,5 +1,3 @@
-var canvas = document.getElementById("circleCanvas");
-var context = canvas.getContext("2d");
-context.arc(50, 50, 50, 0, Math.PI * 2, false);
-context.fillStyle = "red";
-context.fill();
+function myFunction() {
+    alert("Check Email for verification");
+  }
